68fc2ef32d1eb562f19d5a738f199a9ab0c62a81
AjayVishnuE <ajayvishnu121@gmail.com>
2026-05-14 22:28:15 +0530

    basic user structuring

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cors = require('cors');
 
 const connectDB = require('./config/db');
 
@@ -8,6 +9,14 @@ dotenv.config();
 connectDB();
 
 const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+const userRoutes = require('./routes/userRoutes');
+
+app.use('/api/users', userRoutes);
 
 app.get('/', (req, res) => {
     res.send('Backend Running');
