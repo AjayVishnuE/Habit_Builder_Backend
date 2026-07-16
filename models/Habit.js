4017fc5c3ee88345db9a0071cd447ab8f49ec270
AjayVishnuE <ajayvishnu121@gmail.com>
2026-07-16 22:26:42 +0530

    cleaning

--- models/Habit.js
+++ models/Habit.js
@@ -1,28 +1,23 @@
 const mongoose = require('mongoose');
 
 const habitSchema = mongoose.Schema({
-
     user: {
         type: mongoose.Schema.Types.ObjectId,
         required: true,
         ref: 'User'
     },
-
     title: {
         type: String,
         required: true
     },
-
     description: {
         type: String
     },
-
     completedDates: [
         {
             type: Date
         }
     ]
-
 }, {
     timestamps: true
 });
